e8ef9f31a7b3f8d95224dbd193674268126be441
NadymVanSchaik <34303676+NadymVanSchaik@users.noreply.github.com>
2022-06-21 16:31:53 +0200

    Removed unnecessary connection

--- server.js
+++ server.js
@@ -28,47 +28,46 @@ app.get('/', (req, res) => {
     res.send('we are on home');
 })
 
-mongoose.connect(process.env.DB_CONNECTION) 
-.then(() =>{
-    console.log("connected to DB");
-})
-.catch(err => {
-    console.log(err)
-})
+mongoose.connect(process.env.DB_CONNECTION)
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 
 app.get('/', (req, res) => {
     res.status(200).send('hello world');
 });
 
-app.listen(config.port, config.host, (e)=> {
-    if(e) {
+app.listen(config.port, config.host, (e) => {
+    if (e) {
         throw new Error('Internal Server Error');
     }
     logger.info(`${config.name} running on ${config.host}:${config.port}`);
 });
 
 var amqp = require('amqplib/callback_api');
-const CONN_URL = process.env.MQ_CONNECTION; 
+const CONN_URL = process.env.MQ_CONNECTION;
 
-const deleteUser = require('./functions/deleteUser');
 amqp.connect(CONN_URL, function (err, conn) {
+    const deleteUser = require('./functions/deleteUser');
+
     conn.createChannel(function (err, ch) {
         ch.consume('delete-user', function (msg) {
             ch.ack(msg)
             deleteUser(msg.content.toString())
-      },{ noAck: false }
-    );
+        }, { noAck: false }
+        );
     });
-});
+    const updateUser = require('./functions/updateUser');
 
-const updateUser = require('./functions/updateUser');
-amqp.connect(CONN_URL, function (err, conn) {
     conn.createChannel(function (err, ch) {
         ch.consume('update-user', function (msg) {
             ch.ack(msg)
             updateUser(msg.content.toString())
-      },{ noAck: false }
-    );
+        }, { noAck: false }
+        );
     });
 });
